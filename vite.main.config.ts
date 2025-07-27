a562ec7be26514cd90857788b2102134f990883a
Dhemy <imdhemy@gmail.com>
2025-07-27 18:28:31 +0200

    chore: use ESM module

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,4 +4,16 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        lib: {
+            formats: ['es'],
+            entry: 'src/main.ts',
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: 'main.js'
+            }
+        }
+    }
+});
--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -4,4 +4,14 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        lib: {
+            formats: ['es'],
+            entry: 'src/preload.ts',
+        },
+        rollupOptions: {
+            output: { entryFileNames: 'preload.js' }
+        }
+    }
+});
