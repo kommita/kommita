f496d6daa3e31bb9a08ee3ac8851511790883bc9
Dhemy <imdhemy@gmail.com>
2025-07-27 18:58:29 +0300

    chore(ui) Install tailwind css (#3)

--- src/Ui/App.tsx
+++ src/Ui/App.tsx
@@ -1,3 +1,3 @@
 export function App() {
-    return <h1>Hello world!</h1>;
+    return <h1 className='text-3xl font-bold underline'>Hello, Tailwind!</h1>;
 }
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,5 +1,6 @@
 // To learn more about the differences between the "main" and the "renderer" context in Electron, visit:
 // https://electronjs.org/docs/tutorial/process-model
+import './Ui/index.css';
 import { createRoot } from 'react-dom/client';
 import { App } from './Ui/App';
 
