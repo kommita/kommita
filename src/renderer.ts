49b94bc8044c952e1ceb6eaf1f99d0e56ac717a5
Dhemy <imdhemy@gmail.com>
2025-07-27 14:41:24 +0200

    chore: initial commit

---
+++ src/renderer.ts
@@ -0,0 +1,3 @@
+// To learn more about the differences between the "main" and the "renderer" context in Electron, visit:
+// https://electronjs.org/docs/tutorial/process-model
+import './index.css';
