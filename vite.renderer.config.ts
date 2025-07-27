537a760b0041c3edf83bfa952c15d7d81a012d41
Dhemy <imdhemy@gmail.com>
2025-07-27 19:37:04 +0300

    chore: migrate to ESM (#4)

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,14 +3,12 @@
 
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
-import tailwindPostCssPlugin from '@tailwindcss/postcss';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
-    plugins: [viteReact()],
-    css: {
-        postcss: {
-            plugins: [tailwindPostCssPlugin]
-        }
-    },
+    plugins: [
+        viteReact(),
+        tailwindcss(),
+    ],
 });
