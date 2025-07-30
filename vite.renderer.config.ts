ec719dd5d3136d1aff136ce2c78ff238c3863b2a
Dhemy <imdhemy@gmail.com>
2025-07-30 22:21:32 +0200

    add removed plugin

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,10 +3,12 @@
 
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import viteReact from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        viteReact(),
     ],
 });
