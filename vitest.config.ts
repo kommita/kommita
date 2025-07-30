424a3649304d95a69861618b40dcd69e9425d162
Dhemy <imdhemy@gmail.com>
2025-07-30 22:05:46 +0200

    wip: try coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,10 +31,10 @@ export default defineConfig({
         //@ts-ignore
         coverage: {
             thresholds: {
-                // statements: 100,
-                // functions: 100,
-                // branches: 100,
-                // lines: 100,
+                statements: 100,
+                functions: 100,
+                branches: 100,
+                lines: 100,
             },
             include: [
                 'src/core/**/*.ts',
