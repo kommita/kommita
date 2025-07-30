493c546378cad07ce1ad9280c52ca000bbf410ab
Dhemy <imdhemy@gmail.com>
2025-07-30 22:25:41 +0200

    Update vite.renderer.config.ts

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,13 +2,13 @@
 // @ts-nocheck
 
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         viteReact(),
+        tailwindcss(),
     ],
 });
