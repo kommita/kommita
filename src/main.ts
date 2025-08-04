eaa9ff850e5936776e6a33f58ca28f59126e55f3
Dhemy <imdhemy@gmail.com>
2025-08-04 16:05:24 +0200

    feat(window): handle app quit when all windows are closed

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-import { quit, reCreateWindow } from './core/MainWindow';
+import { reCreateWindow } from './core/MainWindow';
 import { handleStartup } from './core/Application/Startup';
 import { app, shouldQuit } from './core/Framework/App';
 import { createWindow } from './core/Application/CreateAppWindow';
 import { windowFactory } from './core/Framework/Window';
 import { partial } from 'ramda';
 import { appEnv } from '../config/AppConfig';
+import { quit } from './core/Application/AppQuit/quit';
+import { Platform } from './core/Application';
 
 handleStartup(shouldQuit, app);
 
 const createAppWindow = partial(createWindow, [windowFactory]);
+const quitApp = partial(quit, [app]);
+const platform: Platform = process.platform as Platform;
 
 app.on('ready', () => createAppWindow({ openDevTools: appEnv !== 'production' }));
-app.on('window-all-closed', quit);
+app.on('window-all-closed', () => quitApp(platform));
 app.on('activate', reCreateWindow);
