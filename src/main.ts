22a6cf44d154391f68c7b54f94bf3839a40dca55
Dhemy <imdhemy@gmail.com>
2025-08-04 15:46:15 +0200

    fix(window): open dev tools only if required

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { quit, reCreateWindow } from './core/MainWindow';
 import { handleStartup } from './core/Application/Startup';
 import { app, shouldQuit } from './core/Framework/App';
-import { partial } from 'ramda';
 import { createWindow } from './core/Application/CreateAppWindow';
 import { windowFactory } from './core/Framework/Window';
-import { openDevTools } from '../config/AppConfig';
+import { partial } from 'ramda';
+import { appEnv } from '../config/AppConfig';
 
 handleStartup(shouldQuit, app);
 
-const createAppWindow = partial(createWindow, [windowFactory, { openDevTools }]);
+const createAppWindow = partial(createWindow, [windowFactory]);
 
-app.on('ready', createAppWindow);
+app.on('ready', () => createAppWindow({ openDevTools: appEnv !== 'production' }));
 app.on('window-all-closed', quit);
 app.on('activate', reCreateWindow);
