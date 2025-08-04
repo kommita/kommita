4d73dad38c00bdd193bab46a3865ce4913aa76dc
Dhemy <imdhemy@gmail.com>
2025-08-04 15:53:13 +0200

    chore(window): implement app quit handler

---
+++ src/core/Application/AppQuit/quit.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test, vi } from 'vitest';
+import { App } from '../types';
+import { quit } from './quit';
+
+describe('App quit', () => {
+    test('it should quit the app on non-darwin platforms', () => {
+        const app: App = { quit: vi.fn() } as unknown as App;
+
+        quit(app, 'win32');
+
+        expect(app.quit).toHaveBeenCalledTimes(1);
+    });
+
+    test('it should not quit the app on darwin platform', () => {
+        const app: App = { quit: vi.fn() } as unknown as App;
+
+        quit(app, 'darwin');
+
+        expect(app.quit).not.toHaveBeenCalled();
+    });
+});
