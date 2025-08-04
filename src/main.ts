08d9fea858e91195ffa50f003c267ad7ea80c74a
Dhemy <imdhemy@gmail.com>
2025-08-04 16:41:19 +0200

    chore(main-window): unify main window options

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { handleStartup } from './core/Application/Startup';
 import { app, shouldQuit } from './core/Framework/App';
-import { createWindow, quit, reCreateMainWindow } from './core/Application/MainWindow';
+import { createWindow, quit, reCreateMainWindow, WindowOptions } from './core/Application/MainWindow';
 import { windowFactory } from './core/Framework/Window';
 import { partial } from 'ramda';
 import { appEnv } from '../config/AppConfig';
@@ -9,13 +9,16 @@ import { BrowserWindow } from 'electron';
 
 handleStartup(shouldQuit, app);
 
-const createAppWindow = partial(createWindow, [windowFactory]);
-
-const quitApp = partial(quit, [app]);
 const platform: Platform = process.platform as Platform;
 
-const activateAppWindow = partial(reCreateMainWindow, [createAppWindow, { openDevTools: appEnv !== 'production' }]);
+const windowOptions: WindowOptions = {
+    openDevTools: appEnv !== 'production',
+};
 
-app.on('ready', () => createAppWindow({ openDevTools: appEnv !== 'production' }));
+const createAppWindow = partial(createWindow, [windowFactory]);
+const quitApp = partial(quit, [app]);
+const activateAppWindow = partial(reCreateMainWindow, [createAppWindow, windowOptions]);
+
+app.on('ready', () => createAppWindow(windowOptions));
 app.on('window-all-closed', () => quitApp(platform));
 app.on('activate', () => activateAppWindow(BrowserWindow.getAllWindows().length));
