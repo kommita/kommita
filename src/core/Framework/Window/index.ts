18068de920c8b4c43a156fb2a7ac705c97739097
Dhemy <imdhemy@gmail.com>
2025-08-05 02:45:45 +0200

    chore(window): separate default window options

--- src/core/Framework/Window/index.ts
+++ src/core/Framework/Window/index.ts
@@ -1,26 +1,10 @@
-import { CreateWindowOptions, WindowMaker, WindowOptions } from './types';
-import { appEnv } from '../../../../config/AppConfig';
-import path from 'node:path';
-import { rootDir } from '../../../../config/FileSystem';
+import { CreateWindowOptions, WindowMaker } from './types';
 import { BrowserWindow } from 'electron';
 import { partial } from 'ramda';
 import { createElectronWindow } from './WindowFactory';
 import { openDevToolsHandler, openHandler } from './WindowHelper';
 import { WindowFactory } from '../../Application/MainWindow';
-
-const defaultWindowOptions: WindowOptions = {
-    windowConstructorOptions: {
-        width: 600,
-        height: 400,
-        webPreferences: {
-            preload: path.join(rootDir, 'preload.js'),
-        },
-    },
-    isDev: appEnv === 'development',
-    openDevTools: false,
-    devServerUrl: MAIN_WINDOW_VITE_DEV_SERVER_URL,
-    mainWindowURL: path.join(rootDir, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
-};
+import { defaultWindowOptions } from './DefaultWindowOptions';
 
 const windowMaker: WindowMaker = (options: CreateWindowOptions) => new BrowserWindow(options);
 
