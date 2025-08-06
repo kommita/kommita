76e222defc68859ec352f916119525118cfb2b1c
Dhemy <imdhemy@gmail.com>
2025-08-06 23:05:39 +0200

    chore(storage): implement patch user setting

---
+++ src/core/Framework/Persistence/UserSetting/PatchUserSetting.ts
@@ -0,0 +1,8 @@
+import { SettingFinder, SettingPersistor } from './types';
+import { UserSetting } from '../../../Domain';
+
+export async function patchUserSetting(find: SettingFinder, persist: SettingPersistor, patch: Partial<UserSetting>): Promise<void> {
+  const currentSetting = await find();
+  const updatedSetting = { ...currentSetting, ...patch };
+  await persist(updatedSetting);
+}
--- src/core/Framework/Persistence/UserSetting/types.ts
+++ src/core/Framework/Persistence/UserSetting/types.ts
@@ -1,5 +1,15 @@
+import { UserSetting } from '../../../Domain';
+
 export interface FileManager {
   writeFile: (path: string, data: string) => void;
   readFile: (path: string) => string;
   exists: (path: string) => boolean;
+}
+
+export interface SettingFinder {
+  (): Promise<UserSetting>;
+}
+
+export interface SettingPersistor {
+  (userSetting: UserSetting): Promise<void>;
 }
