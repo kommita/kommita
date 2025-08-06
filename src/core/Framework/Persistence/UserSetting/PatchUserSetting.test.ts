b5888129909aa8feb9d2a40eb3625083e25390d1
Dhemy <imdhemy@gmail.com>
2025-08-06 23:47:35 +0200

    chore: fix typo

--- src/core/Framework/Persistence/UserSetting/PatchUserSetting.test.ts
+++ src/core/Framework/Persistence/UserSetting/PatchUserSetting.test.ts
@@ -3,7 +3,7 @@ import { SettingFinder, SettingPersistor } from './types';
 import { UserSetting } from '../../../Domain';
 import { patchUserSetting } from './PatchUserSetting';
 
-test('Path user setting', async () => {
+test('Patch user setting', async () => {
   const finder: SettingFinder = vi.fn().mockReturnValue({ window: { width: 100 } }) as unknown as SettingFinder;
   const persistor: SettingPersistor = vi.fn() as unknown as SettingPersistor;
   const patch = { window: { width: 1024, height: 768 } } as unknown as Partial<UserSetting>;
