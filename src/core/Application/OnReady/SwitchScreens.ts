87e9fa002a5c3925f828d1c598e8e5b191df3f40
Dhemy <imdhemy@gmail.com>
2025-08-10 12:56:12 +0200

    chore: rename parameter

--- src/core/Application/OnReady/SwitchScreens.ts
+++ src/core/Application/OnReady/SwitchScreens.ts
@@ -3,12 +3,12 @@ import { AppWindow } from '../types';
 export async function switchScreens(
   main: AppWindow,
   splash: AppWindow,
-  wait: () => Promise<void>
+  simulateLoadingTime: () => Promise<void>
 ): Promise<void> {
   await splash.open();
-  await wait();
-  await main.open();
+  await simulateLoadingTime();
 
+  await main.open();
   main.on('ready-to-show', async () => {
     main.show();
     splash.close();
