ae5c934722c10f53025cf527114358a8fddd53bb
Dhemy <imdhemy@gmail.com>
2025-08-10 17:39:16 +0200

    chore(test): fix storybook style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
+import '../src/ui/index.css';
+
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -12,7 +14,7 @@ const preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: "todo"
+      test: 'todo'
     }
   },
 };
