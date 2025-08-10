240a8a7f4ec8a0e085e0852322a47e9e991debfd
Dhemy <imdhemy@gmail.com>
2025-08-10 14:18:32 +0200

    chore(ui): enable react strict mode

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,3 +1,9 @@
+import { StrictMode } from 'react';
+
 export function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello, Tailwind!</h1>;
+  return (
+    <StrictMode>
+      <h1 className='text-3xl font-bold text-center underline'>Kommita!</h1>
+    </StrictMode>
+  );
 }
