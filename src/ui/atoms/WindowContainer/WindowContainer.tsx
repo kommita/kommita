330a1d8b3cc5d1829062f8366085382a7fa58193
Dhemy <imdhemy@gmail.com>
2025-08-10 23:25:22 +0200

    chore(theme): style window container

--- src/ui/atoms/WindowContainer/WindowContainer.tsx
+++ src/ui/atoms/WindowContainer/WindowContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function WindowContainer(props: Props) {
   return (
-    <div className='h-screen flex flex-col bg-[#2e3440] text-white font-inter'>
+    <div className='h-screen flex flex-col bg-dark text-primary font-inter text-sm'>
       {props.children}
     </div>
   );
