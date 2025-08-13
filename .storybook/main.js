ca125a177d0ad3ca0b0c6b14f64376ea10cba51a
Dhemy <imdhemy@gmail.com>
2025-08-13 15:17:33 +0200

    chore(storybook): use conventional ending

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 const config = {
   'stories': [
     '../src/ui/**/*.mdx',
-    '../src/ui/**/*.story.@(js|jsx|mjs|ts|tsx)'
+    '../src/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   'addons': [
     '@chromatic-com/storybook',
