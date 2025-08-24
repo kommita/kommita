1d8d31fc26585ab9a4ddbc858529e9d6427786b9
Dhemy <imdhemy@gmail.com>
2025-08-24 23:28:29 +0200

    chore: make tests type safe

--- src/core/Application/OnResize/StoreWindowSize.test.ts
+++ src/core/Application/OnResize/StoreWindowSize.test.ts
@@ -1,18 +1,19 @@
 import { describe, expect, test, vi } from 'vitest';
-import { UserSettingRepository } from '../../Domain';
-import { AppWindow } from '../types';
 import { storeWindowSize } from './StoreWindowSize';
+import { AppWindow } from '../types';
+import { UserSettingRepository } from '../../Domain';
 
 describe('Store window size', () => {
   test('it should store window size', async () => {
-    const appWindow: vi.mocked<AppWindow> = {
-      getSize: vi.fn<AppWindow['getSize']>().mockReturnValue({ width: 800, height: 600 }),
+    const appWindow: Pick<AppWindow, 'getSize'> = {
+      getSize: vi.fn().mockReturnValue({ width: 800, height: 600 }),
     };
-    const settingsRepository: vi.mocked<UserSettingRepository> = {
-      patch: vi.fn<UserSettingRepository['patch']>(),
+    const settingsRepository: Pick<UserSettingRepository, 'patch'> = {
+      patch: vi.fn(),
     };
 
-    await storeWindowSize(settingsRepository, appWindow);
+    await storeWindowSize(settingsRepository as UserSettingRepository, appWindow as AppWindow);
+    
     expect(settingsRepository.patch).toHaveBeenCalledWith({ window: { width: 800, height: 600 } });
   });
 });
