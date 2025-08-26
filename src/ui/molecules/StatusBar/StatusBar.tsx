d4951d5f15e4407403c818c68779bdd1929afe42
Dhemy <imdhemy@gmail.com>
2025-08-26 21:42:02 +0200

    chore: remove extra space

--- src/ui/molecules/StatusBar/StatusBar.tsx
+++ src/ui/molecules/StatusBar/StatusBar.tsx
@@ -3,7 +3,7 @@ import { version } from '../../../../package.json';
 
 export function StatusBar() {
   return (
-    <div className='flex items-center h-8 px-4  bg-surface text-secondary text-xs absolute bottom-0 w-full'>
+    <div className='flex items-center h-8 px-4 bg-surface text-secondary text-xs absolute bottom-0 w-full'>
       <span className='ml-auto'>{version}</span>
     </div>
   );
